589ef84da9d1c06bf532561ac284326a15c5903e
Tung Le <thanhtung3496@gmail.com>
2020-03-22 18:18:29 -0400

    first commit

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>(){
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Location = "MaryLand", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Budhha Barn", Location = "NewPort", Cuisine = CuisineType.Indian},
+                new Restaurant {Id = 3, Name = "Taco Bell", Location = "Cincy", Cuisine = CuisineType.Mexican}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll() => from r in restaurants orderby r.Name select r;
+    }
+}
